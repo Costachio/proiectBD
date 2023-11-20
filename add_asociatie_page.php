ce4ef6ba85835f1a643bfc8a74a9053f8c78a66d
Andrei <andrei.costache2@student.tuiasi.ro>
2023-11-21 00:21:41 +0200

    - main menu page and asociatie page and add new asociatie page - css

--- add_asociatie_page.php
+++ add_asociatie_page.php
@@ -1,25 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Add song</title>
+    <title>Adauga asociatie</title>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
 <div class="container">
-    <h1>Add new song</h1>
+    <h1>Adauga o noua asociatie</h1>
     <form method="post" action="add_asociatie_in_database.php">
         <div class="mb-3">
-            <label for="video_id" class="form-label">Video id</label>
-            <input required type="text" name="video_id" class="form-control" id="video_id">
+            <label for="denumire" class="form-label">Denumire</label>
+            <input required type="text" name="denumire" class="form-control" id="denumire">
         </div>
         <div class="mb-3">
-            <label for="start" class="form-label">Start</label>
-            <input required type="number" name="start" class="form-control" id="start">
+            <label for="nume_administrator" class="form-label">Nume administrator</label>
+            <input required type="text" name="nume_administrator" class="form-control" id="nume_administrator">
         </div>
         <div class="mb-3">
-            <label for="end" class="form-label">End</label>
-            <input required type="number" name="end" class="form-control" id="end">
+            <label for="nume_presedinte" class="form-label">Nume presedinte</label>
+            <input required type="text" name="nume_presedinte" class="form-control" id="nume_presedinte">
+        </div>
+        <div class="mb-3">
+            <label for="nume_contabil" class="form-label">Nume contabil</label>
+            <input required type="text" name="nume_contabil" class="form-control" id="nume_contabil">
+        </div>
+        <div class="mb-3">
+            <label for="cod_fiscal" class="form-label">Cod fiscal</label>
+            <input required type="text" name="cod_fiscal" class="form-control" id="cod_fiscal">
+        </div>
+        <div class="mb-3">
+            <label for="data_infiintare" class="form-label">Data infiintare ( exemplu format: 20/12/2023 )</label>
+            <input required type="text" name="data_infiintare" class="form-control" id="data_infiintare">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
--- index.php
+++ index.php
@@ -1,42 +1,25 @@
-<?php
-
-require_once "connect.php";
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Songs</title>
+    <title>Asociatii</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://www.youtube.com/iframe_api"></script>
     <script src="js/admin.js"></script>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 <div class="container">
-    <h1>Administreaza asociatia</h1>
-    <div class="mt-3 mb-3">
-        <a href="add_asociatie_page.php" class="btn btn-primary"><i class="bi bi-plus"></i></a>
-    </div>
-    <table class="table">
-        <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">ID</th>
-            <th scope="col">Denumire</th>
-            <th scope="col">Cod fiscal</th>
-            <th scope="col">Data infiintare</th>
-        </tr>
-        </thead>
-        <tbody>
-
-        </tbody>
-    </table>
-    <div class="pt-3">
-        <div id="player"></div>
-    </div>
+    <h2 class="title">Managementul asociatiilor de administrare blocuri</h2>
+    <ul class="main-menu">
+        <li class="row-btn"><a href="asociatii_page.php" class="btn btn-primary button">Asociatii</a></li>
+        <li class="row-btn"><a href="#" class="btn btn-primary button">Conturi bancare</a></li>
+        <li class="row-btn"><a href="#" class="btn btn-primary button">Imobile</a></li>
+        <li class="row-btn"><a href="#" class="btn btn-primary button">Scari</a></li>
+        <li class="row-btn"><a href="#" class="btn btn-primary button">Apartamente</a></li>
+        <li class="row-btn"><a href="#" class="btn btn-primary button">Persoane</a></li>
+    </ul>
 </div>
 </body>
 </html>
